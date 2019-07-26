03366bd603fc2d1fc2f758c1bfed0c746b1608f1
Mo Beigi <me@mobeigi.com>
2019-07-26 16:45:02 +1000

    Add versioning and meta data files and GPL header

---
+++ src/__init__.py
@@ -0,0 +1,27 @@
+""" 
+    fb2cal - Facebook Birthday Events to ICS file converter
+    Created by: mobeigi
+
+    This program is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from _version import __version_info__, __version__
+
+__author__ = 'Mo Beigi'
+__copyright__ = 'Copyright 2019'
+__email__ = 'me@mobeigi.com'
+__license__ = "GPLv3"
+__maintainer__ = 'Mo Beigi'
+__status__ = "Production"
+
+# Make metadata public to script
+__all__ = ['__author__', '__copyright__', '__email__', '__license__', '__maintainer__', '__status__', '__version_info__', '__version__']
--- src/fb2cal.py
+++ src/fb2cal.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
 """ 
-    fb2cal
-    Created by: mobeigi (mobeigi.com)
+    fb2cal - Facebook Birthday Events to ICS file converter
+    Created by: mobeigi
+
+    This program is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <http://www.gnu.org/licenses/>.
 """
+
+from __init__ import *
 
 import os
 import sys
@@ -360,5 +373,3 @@ def populate_birthdays_calendar(birthdays):
 
 if __name__ == "__main__":
     main()
-
-__version__ = '1.0.0'
