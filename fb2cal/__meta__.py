9c576b794f9041d877a224f55f6393964f092883
Mo Beigi <me@mobeigi.com>
2020-11-13 19:00:59 +1100

    Bump version to v1.2.1

--- fb2cal/__meta__.py
+++ fb2cal/__meta__.py
@@ -5,7 +5,7 @@ __license__ = "GPLv3"
 __maintainer__ = 'Mo Beigi'
 __status__ = 'Production'
 __website__ = 'https://git.io/fjMwr'
-__version_info__ = (1, 2, 0)
+__version_info__ = (1, 2, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 
