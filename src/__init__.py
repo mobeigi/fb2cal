d905152e63346d611186a99eab9f8781a14d0564
Mo Beigi <me@mobeigi.com>
2020-11-13 17:23:08 +1100

    Refactor to introduce __meta__ file

--- src/__init__.py
+++ src/__init__.py
@@ -14,15 +14,4 @@
     this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from _version import __version_info__, __version__
-
-__author__ = 'Mo Beigi'
-__copyright__ = 'Copyright 2019'
-__email__ = 'me@mobeigi.com'
-__license__ = "GPLv3"
-__maintainer__ = 'Mo Beigi'
-__status__ = 'Production'
-__website__ = 'https://git.io/fjMwr'
-
-# Make metadata public to script
-__all__ = ['__author__', '__copyright__', '__email__', '__license__', '__maintainer__', '__status__', '__website__', '__version_info__', '__version__']
+from .__meta__ import *
