7c67f04ccc2915248299a5f23662a4e1bba2ef3c
Mo Beigi <me@mobeigi.com>
2023-12-25 13:45:49 +1100

    Update meta data around Python version and also pypi

--- fb2cal/__meta__.py
+++ fb2cal/__meta__.py
@@ -2,14 +2,16 @@ __author__ = 'Mo Beigi'
 __copyright__ = 'Copyright 2019-2020'
 __description__ = 'Facebook Birthday Events to ICS file converter'
 __email__ = 'me@mobeigi.com'
+__keywords__ = ['facebook', 'birthday', 'calendar', 'export', 'ics']
 __license__ = "GPLv3"
 __maintainer__ = 'Mo Beigi'
 __status__ = 'Production'
 __title__ = 'fb2cal'
-__website__ = 'https://git.io/fjMwr'
 __version_info__ = (1, 2, 4)
 __version__ = '.'.join(map(str, __version_info__))
 
+__website__ = 'https://git.io/fjMwr'
+__download_url__= f'https://github.com/mobeigi/fb2cal/archive/v{__version__}.tar.gz'
 
 # Make metadata public to script
-__all__ = ['__author__', '__copyright__', '__description__', '__email__', '__license__', '__maintainer__', '__status__', '__title__', '__website__', '__version_info__', '__version__']
+__all__ = ['__author__', '__copyright__', '__description__', '__email__', '__license__', '__maintainer__', '__status__', '__title__', '__version_info__', '__version__', '__website__', '__download_url__']
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from fb2cal.__meta__ import __title__, __version__, __description__, __license__, __author__, __email__, __website__
+from fb2cal.__meta__ import __title__, __version__, __description__, __license__, __author__, __email__, __min_python_version__, __website__, __download_url__, __keywords__
 
 setup(
     name=__title__,
@@ -10,5 +10,24 @@ setup(
     author=__author__,
     author_email=__email__,
     url=__website__,
+    download_url=__download_url__,
+    keywords=__keywords__,
     python_requires='>3.9.0',
+    install_requires=[
+        'MechanicalSoup',
+        'ics>=0.6',
+        'requests',
+        'freezegun',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Other Audience',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
 )
