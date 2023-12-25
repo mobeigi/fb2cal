c855f9772c8ec45d7a887ee5d9d888b777a780f8
Mo Beigi <me@mobeigi.com>
2023-12-25 14:51:34 +1100

    Remove unused import

--- fb2cal/facebook_browser.py
+++ fb2cal/facebook_browser.py
@@ -13,7 +13,6 @@ class FacebookBrowser:
         self.browser = mechanicalsoup.StatefulBrowser()
         self.browser.set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
         self.__cached_token = None
-        self.__cached_locale = None
 
     def authenticate(self, email, password):
         """ Authenticate with Facebook setting up session for further requests """
