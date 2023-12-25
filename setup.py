b325a799353a0d570e3588421b2fd3690ed073e2
Mo Beigi <me@mobeigi.com>
2023-12-25 16:27:56 +1100

    Add long description for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
+from os import path
+import re
 from setuptools import setup, find_packages
-from fb2cal.__meta__ import __title__, __version__, __description__, __license__, __author__, __email__, __website__, __download_url__, __keywords__
+
+from fb2cal.__meta__ import __title__, __version__, __description__, __license__, __author__, __email__, __github_url__, __github_short_url__, __github_assets_absolute_url__, __download_url__, __keywords__
+
+def read(fname, base_url, base_image_url):
+    """Read the content of a file."""
+    with open(path.join(path.dirname(__file__), fname)) as fd:
+        readme = fd.read()
+    if hasattr(readme, 'decode'):
+        # In Python 3, turn bytes into str.
+        readme = readme.decode('utf8')
+    # turn relative links into absolute ones
+    readme = re.sub(r'`<([^>]*)>`__',
+                    r'`\1 <' + base_url + r"/blob/master/\1>`__",
+                    readme)
+    readme = re.sub(r"\.\. image:: /", ".. image:: " + base_image_url + "/", readme)
+
+    return readme
 
 setup(
     name=__title__,
@@ -9,7 +27,7 @@ setup(
     license=__license__,
     author=__author__,
     author_email=__email__,
-    url=__website__,
+    url=__github_short_url__,
     download_url=__download_url__,
     keywords=__keywords__,
     python_requires='>3.9',
@@ -19,6 +37,8 @@ setup(
         'requests',
         'freezegun',
     ],
+    long_description=read('README.md', __github_url__, __github_assets_absolute_url__),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Other Audience',
